3acde04ecb1e6d9165d121b72032755b87597eed
crnjacdaniel-commits <crnjacdaniel@gmail.com>
2025-11-23 22:03:47 +0100

    Update json.js

--- json.js
+++ json.js
@@ -40,24 +40,7 @@ const json = {
           "name": "question4",
           "choices": [
             "Item 1",
-            "Item 2",
-             "elements"[      {
-          "type": "dropdown",
-          "name": "question1",
-          "title": "Für welches Land stimmen Sie ab?",
-          "isRequired": true,
-          "choices": [
-            {
-              "value": "Item 1",
-              "text": "Deutschland"
-            },
-            {
-              "value": "Item 2",
-              "text": "Finnland"
-            }
-          ],
-          "choicesOrder": "asc"
-        }]
+            "Item 2"
           ]
         }
       ]
